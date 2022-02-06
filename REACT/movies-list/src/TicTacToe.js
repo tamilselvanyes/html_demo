2c4712e0249cecf8bd9ed2425501aabcb64df2e3
tamilselvanyes <tamilselvanyes@gmail.com>
2022-02-06 09:16:58 +0530

    scorecard, celebration implementation

--- REACT/movies-list/src/TicTacToe.js
+++ REACT/movies-list/src/TicTacToe.js
@@ -1,8 +1,16 @@
 import { useState } from 'react';
+import Button from '@mui/material/Button';
+import useWindowSize from 'react-use/lib/useWindowSize'
+import Confetti from 'react-confetti'
 
 const INITIAL_STATES = [null, null, null, null, null, null, null, null, null]
 export function TicTacToe() {
     const [board, setBoard] = useState(INITIAL_STATES);
+    const winner = decideWinner(board);
+    const [xwin, setXwin] = useState(0);
+    const [owin, setOwin] = useState(0);
+    const [draw, setDraw] = useState(0);
+    const { width, height } = useWindowSize()
 
     function decideWinner(board) {
         const lines = [
@@ -21,50 +29,86 @@ export function TicTacToe() {
             const [a, b, c] = lines[i];
 
             if (board[a] !== null && board[a] === board[b] && board[b] === board[c]) {
-                return `won by ${board[a]}`;
-
+                console.log("Winner declared");
+                return board[a];
             }
         }
 
-        for(let j = 0; j < board.length; j++) {
+        for (let j = 0; j < board.length; j++) {
             //Draw logic 
-            if(board[j] !== null){
-                    count_null = count_null + 1;
+            if (board[j] !== null) {
+                count_null = count_null + 1;
             }
             // when count is 9 that means the box is full but still no winner which means "DRAW"
-            if(count_null === 9){
+            if (count_null === 9) {
                 return "DRAW";
             }
         }
-        
+
         return null;
     }
 
-    const winner = decideWinner(board);
+
     const [isXturn, setIsXturn] = useState(true);
+    var result = "";
+    if (winner === "DRAW") {
+        result = "DRAW";
+    } else {
+        result = `won by ${winner}`
+    }
+
+
+
 
     const handleClick = (index) => {
         if (winner === null && board[index] === null) {
             const boardCopy = [...board];
             boardCopy[index] = isXturn ? "X" : "O";
             setBoard(boardCopy);
+            if (decideWinner(boardCopy) === "X") {
+                setXwin(xwin + 1);
+            }
+            if (decideWinner(boardCopy) === "O") {
+                setOwin(owin + 1);
+            }
+            if (decideWinner(boardCopy) === "DRAW") {
+                setDraw(draw + 1);
+            }
             setIsXturn(!isXturn);
         }
 
     };
+
+
     return (
         <div className="full-game">
+            {(winner && winner !== "DRAW") ? <Confetti
+                width={width}
+                height={height}
+            />:""}
             <div className="board-info">
+                <div className="scoreboard">
+                    <h3>Score Card</h3>
+                    <span>X's win:<b>{xwin}</b></span><br></br>
+                    <span>O's win:<b>{owin}</b></span>
+                    <p>Draw: {draw}</p>
+                    <Button variant="outlined" onClick={() => {
+                        setBoard(INITIAL_STATES)
+                        setXwin(0);
+                        setOwin(0)
+                        setDraw(0)
+                    }}>Restart Series</Button>
+                </div>
                 <div className="board">
                     {board.map((val, index) => (
                         <Gamebox key={index} val={val} onPlayerClick={() => handleClick(index)} />))}
                 </div>
-                <div>
+                <div className="turn-details">
                     {isXturn ? <h4>X's Turn</h4> : <h4>O's Turn</h4>}
                 </div>
             </div>
             {winner ? <h2 className="result">
-                Match {winner} <button className="btn btn-primary" onClick = {()=>setBoard(INITIAL_STATES)} > Restart </button></h2> : ""}
+                Match {result} <Button variant="outlined" className="success" onClick={() => setBoard(INITIAL_STATES)} > Restart </Button></h2> : ""}
         </div>
     );
 }
@@ -83,4 +127,7 @@ function Gamebox({ val, onPlayerClick }) {
     return (
         <div className="gamebox" style={styles} onClick={onPlayerClick}>{val}</div>
     );
+
+
 }
+
